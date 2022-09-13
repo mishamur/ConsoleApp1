723d0810403bcb2fe698c73bb678b6cba9440d95
Михаил Котов <misha.kotov.01@list.ru>
2022-09-13 21:09:12 +0400

    commit 1

---
+++ ConsoleApp1/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class TestClass
+    {
+        public static void PrintConsole()
+        {
+            Console.WriteLine("maximin");
+        }
+    }
+}
